4cb6ba290e4e6be0d5cfeea7d18c4f894ad54650
xMxRxxx <muhammadrivan888@gmail.com>
2023-09-14 19:38:51 +0700

    portofolio

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useRef } from "react";
 
 const Resume = () => {
   
-
-
   return (
     <iframe src="https://drive.google.com/file/d/1bjbcNG0NSAWP0p9D8VLUPfoCNCIiaSsu/preview" frameborder="0" className="w-full h-screen"></iframe>
   );
